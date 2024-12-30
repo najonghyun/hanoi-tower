7c270f8126fd976a5f9f9bbc854156aeddd8e99b
najonghyun <i0364842@naver.com>
2024-12-30 15:07:15 +0900

    init: vue 프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+}
+</style>
