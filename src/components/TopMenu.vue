04d102d3f3e460cff117156faff15503fca14075
najonghyun <i0364842@naver.com>
2025-01-03 14:07:39 +0900

    feat: 하노이 알고리즘 부분 구현, next, auto 포함

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -12,17 +12,28 @@
       </form>
     </div>
     <div class="menu-step">
-      <button class="menu-step">Step</button>
-      <button class="menu-auto">Auto</button>
+      <button class="menu-step" @click="nextStep">Step</button>
+      <button class="menu-auto" @click="autoStep">Auto</button>
     </div>
+    {{ stack1.items }}
+    {{ stack2.items }}
+    {{ stack3.items }}
   </div>
 </template>
   <script>
 import { mapMutations } from "vuex";
+import Stack from "../utils/stack.js";
+
 export default {
   data() {
     return {
       inputNumber: "",
+      count: 0,
+      finish: false,
+      stack1: new Stack(),
+      stack2: new Stack(),
+      stack3: new Stack(),
+      task: new Stack(),
     };
   },
   methods: {
@@ -30,7 +41,49 @@ export default {
     onSubmit() {
       const temp = this.inputNumber;
       this.SET_NUMBER(temp);
+      const number = Number(temp);
+      for (let i = number; i > 0; i--) {
+        this[`stack1`].push(i);
+      }
+      // task : 초기 상태 push
+      this.task.push([number, 1, 2, 3]);
       this.inputNumber = "";
+    },
+    nextStep() {
+      if (this.task.isEmpty()) {
+        this.finish = true;
+        console.log("모든 작업 끝");
+        return;
+      }
+      const n = this.task.peek()[0];
+      const from = this.task.peek()[1];
+      const temp = this.task.peek()[2];
+      const to = this.task.peek()[3];
+      this.task.pop();
+
+      if (n == 1) {
+        this.move(from, to);
+        this.count++;
+        console.log("Step " + this.count + ": " + from + "->" + to);
+      } else {
+        this.task.push([n - 1, temp, from, to]);
+        this.task.push([1, from, temp, to]);
+        this.task.push([n - 1, from, to, temp]);
+        this.nextStep();
+      }
+    },
+    autoStep() {
+      let interval = setInterval(() => {
+        if (this.finish) {
+          clearInterval(interval);
+          console.log("작업 완료");
+        } else {
+          this.nextStep();
+        }
+      }, 1000);
+    },
+    move(from, to) {
+      this[`stack${to}`].push(this[`stack${from}`].pop());
     },
   },
 };
---
+++ src/utils/stack.js
@@ -0,0 +1,27 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    return this.items.pop();
+  }
+  peek() {
+    if (this.items.length === 0) {
+      return null;
+    }
+
+    return this.items[this.items.length - 1];
+  }
+  getSize() {
+    return this.items.length;
+  }
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+}
+
+export default Stack;
